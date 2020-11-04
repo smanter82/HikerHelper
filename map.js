6d465ac6f89f7fb127a1a227e577132050507b3d
Velma <k.ceb.09@gmail.com>
2020-11-03 21:31:11 -0500

    added map widget to JS

---
+++ map.js
@@ -0,0 +1,5 @@
+//Map Widget Input
+var script = document.createElement("script"); 
+        script.setAttribute("src", "https://es.pinkbike.org/ttl-86400/sprt/j/trailforks/widget.js"); document.getElementsByTagName("head")[0].appendChild(script); 
+        var widgetCheck = false;
+
